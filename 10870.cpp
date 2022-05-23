a6cd847ce61bc82fb35c77700ffb7bc029545502
dohunk <craz1000@naver.com>
2022-05-23 21:56:38 +0900

    22.05.23 study Commit

---
+++ 10870.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+// 피보나치 수
+int Fibonacci(int n) {
+	if(n == 0) {
+		return 0;
+	}
+	else if(n == 1) {
+		return 1;
+	}
+   	else {
+		return (Fibonacci(n-1) + Fibonacci(n-2));
+	}
+}
+int main() {
+	int n;
+	cin >> n;
+
+	cout << Fibonacci(n);
+}
